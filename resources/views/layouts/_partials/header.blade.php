605c7475babe2e54cdf07d58a2ec81af6c39a14a
Jesse Geelissen <65621046+jgeeliss@users.noreply.github.com>
2025-11-22 23:16:45 +0100

    added some classless css

--- resources/views/layouts/_partials/header.blade.php
+++ resources/views/layouts/_partials/header.blade.php
@@ -1,7 +1,9 @@
 <header>
-    <ul>
-        <li><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="{{ route('keyboards.index') }}">Keyboard Layouts</a></li>
-        <li><a href="{{ route('keyboards.create') }}">Add Keyboard</a></li>
-    </ul>
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('keyboards.index') }}">Keyboard Layouts</a></li>
+            <li><a href="{{ route('keyboards.create') }}">Add Keyboard</a></li>
+        </ul>
+    </nav>
 </header>
