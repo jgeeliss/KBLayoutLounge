bb5f7a00ee2d6fec95ac26912030acc649fadf40
Jesse Geelissen <65621046+jgeeliss@users.noreply.github.com>
2025-11-23 09:02:51 +0100

    added basic keyboard grid in create

--- resources/views/keyboards/create.blade.php
+++ resources/views/keyboards/create.blade.php
@@ -32,6 +32,28 @@
         </div>
 
         <div>
+            <label>Keyboard Layout</label>
+            @php
+            $qwertyLayout = [
+                ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+                ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';'],
+                ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/']
+            ];
+            @endphp
+            @foreach($qwertyLayout as $rowIndex => $row)
+            <div style="display: flex; gap: 4px; margin-bottom: 4px; padding-left: {{ $rowIndex * 20 }}px;">
+                @foreach($row as $colIndex => $defaultKey)
+                <select name="layout[{{ $rowIndex }}][{{ $colIndex }}]" style="flex: 1; max-width: 50px; font-size: 12px; {{ $colIndex == 5 ? 'margin-left: 20px;' : '' }}">
+                    @foreach(array_merge(range('A', 'Z'), ['.', ',', '/', ';', '\'']) as $letter)
+                    <option value="{{ $letter }}" {{ $defaultKey == $letter ? 'selected' : '' }}>{{ $letter }}</option>
+                    @endforeach
+                </select>
+                @endforeach
+            </div>
+            @endforeach
+        </div>
+
+        <div>
             <button
                 type="submit">
                 Create Keyboard
