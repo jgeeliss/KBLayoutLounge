430437736fc1896410f43877928fa2c344e449c4
Jesse Geelissen <65621046+jgeeliss@users.noreply.github.com>
2025-12-17 13:11:48 +0100

    added user routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,15 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\KeyboardController;
 use App\Http\Controllers\RatingController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 Route::view('/about', 'about')->name('about');
+
+// User routes
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::post('/users/{user}/toggle-admin', [UserController::class, 'toggleAdmin'])->name('users.toggleAdmin');
 
 // Keyboard routes
 Route::get('/keyboards', [KeyboardController::class, 'index'])->name('keyboards.index');
